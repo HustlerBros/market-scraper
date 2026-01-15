c0c0ce136aeaae24554cea9158372d6e83836340
DixusSamael <154350734+DixusSamael@users.noreply.github.com>
2026-01-15 02:40:18 +0100

    Added health check

--- src/mexc_futures_scraper.py
+++ src/mexc_futures_scraper.py
@@ -54,6 +54,11 @@ def get_db():
 
 
 ###TG
+async def health(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "✅"
+    )
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat = update.effective_chat
     username = update.effective_user.username
@@ -196,6 +201,7 @@ def main():
         app = ApplicationBuilder().token(BOT_TOKEN).build()
 
         app.add_handler(CommandHandler("start", start))
+        app.add_handler(CommandHandler("health", health))
     except Exception as e:
         print(e)
         raise e
